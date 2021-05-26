21260ef46a3697bb74b1dac7ea5f5037bab90fa0
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2021-05-26 16:56:57 +0530

    Create day1.py

---
+++ 30 days of code/day1.py
@@ -0,0 +1,21 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a=12
+b=4.0
+c="is the best place to learn and practice coding!"
+
+# Read and save an integer, double, and String to your variables.
+a=int(input())
+b=float(input())
+c=str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i+a)
+# Print the sum of the double variables on a new line.
+print(d+b)
+# Concatenate and print the String variables on a new line
+con=s+c
+print(con)
+# The 's' variable above should be printed first.
