0da87567c7eabab34a21432dc001829447e75994
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2021-06-24 12:24:07 +0530

    Create maps.cpp

---
+++ CPP/maps.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int main() {
+    int q, type; 
+    cin >> q; 
+    map<string,int> clas;
+     string name;
+    for (int i(0), mark; i<q; ++i)
+    {
+        cin >> type >> name;
+        if (type == 1){
+            cin >> mark;
+            clas[name] += mark;
+        }
+        else if (type == 2)
+            clas.erase(name);
+        else
+            cout << clas[name] << "\n";
+    }
+    return 0;
+}
