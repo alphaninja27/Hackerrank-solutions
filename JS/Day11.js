dd7599ecdfd921a8b8a7562d81e21d61057ef7cb
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2021-08-08 17:28:32 +0530

    Create Day11.js

---
+++ JS/Day11.js
@@ -0,0 +1,12 @@
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area=length*width;
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter=2*(length+width)
+    return perimeter;
