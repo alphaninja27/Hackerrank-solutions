86ee8e845be4563deaf94f766ef102574ea24b1f
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2021-08-08 17:28:29 +0530

    Create Day02.js

---
+++ JS/Day02.js
@@ -0,0 +1,7 @@
+var intSum =  firstInteger + Number(secondInteger);
+    var decSum = firstDecimal + Number(secondDecimal);
+    var strSum = firstString.concat(secondString);
+    
+    console.log(intSum);
+    console.log(decSum);
+    console.log(strSum);
