9f6b2f6c3c0e1f54842dd04643966d74f0ad1a77
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2021-08-08 13:06:01 +0530

    Create MapLambda.py

---
+++ Python/MapLambda.py
@@ -0,0 +1,8 @@
+cube = lambda x: x*x*x
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+    lis = [0,1]
+    for i in range(2,n):
+        lis.append(lis[i-2] + lis[i-1])
+    return(lis[0:n])
