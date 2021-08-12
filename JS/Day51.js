8ae0f641a6881f39757c7f4ab31e1e04b9bfdd21
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2021-08-12 23:27:48 +0530

    Day5

---
+++ JS/Day51.js
@@ -0,0 +1,10 @@
+Rectangle.prototype.area = function() {
+    return(this.w*this.h);
+
+class Square extends Rectangle {
+    constructor(s) {
+        super(s);
+        this.h = s;
+        this.w = s;
+    }
+};
---
+++ JS/Day53.js
@@ -0,0 +1,10 @@
+function modifyArray(nums) {
+    var something = function(n){
+        if(n%2==0)
+            return n*2;
+        else
+            return n*3;
+    }
+    var newArray = nums.map(something);
+    return newArray;
+}
