371464b41fa49435fc35322977918b17507284b0
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2021-08-16 21:16:44 +0530

    Day1

---
+++ Stats/Day13.py
@@ -0,0 +1,8 @@
+n = int(input().strip())
+X = [int(x) for x in input().strip().split()]
+
+mean = sum(X) / n
+variance = sum([((x - mean) ** 2) for x in X]) / n
+stddev = variance ** 0.5
+
+print("{0:0.1f}".format(stddev))
