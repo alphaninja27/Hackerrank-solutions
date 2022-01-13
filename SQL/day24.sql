0cc979a3a2927e4a8d911801b0616849bf0ab7cb
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2022-01-13 18:54:05 +0530

    Create day24.sql

---
+++ SQL/day24.sql
@@ -0,0 +1,6 @@
+select concat(name,'(',upper(substring(occupation,1,1)),')') from occupations
+order by name;
+
+select concat("There are a total of",' ',count(occupation),' ',lower(occupation),'s',".")  from occupations
+group by occupation
+order by count(occupation) asc;
