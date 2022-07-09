66c9f5dab909693d7b489572186b8332904cea28
Armaan Nayyar <72308743+alphaninja27@users.noreply.github.com>
2022-07-10 01:53:02 +0530

    Create Jumping on the Clouds.py

---
+++ Problem Solving/Jumping on the Clouds.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+    if len(c) == 1 : return 0
+    if len(c) == 2: return 0 if c[1]==1 else 1
+    if c[2]==1: 
+        return 1 + jumpingOnClouds(c[1:])
+    if c[2]==0:
+        return 1 + jumpingOnClouds(c[2:])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
